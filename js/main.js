1f96698763c4a99b38f03a92425a242fee6aac9a
Kurpenov Kuat <kurpenov2003@mai.ru>
2020-09-24 22:30:23 +0600

    Add slider with speakers

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ var counter = 0;
 
 document.getElementById("header").style.height = window.innerHeight + "px";
 
+window.addEventListener("mousewheel", function(e) {
+    if (e.ctrlKey) {
+        e.preventDefault();
+        return false;
+    }
+});
+
 function menu(params) {
     if (counter == 0){
         var menu = document.getElementById("menu");
@@ -24,7 +31,6 @@ function menu(params) {
         var modal = document.getElementById("modal-menu")
         modal.style.display = "none";
         menu.style.zIndex = "999";
-        modal.style.zIndex = "1";
         menu.src = "images/menu.png";
         head.style.transitionProperty = "filter";
         head.style.transitionDuration = "250ms";
@@ -50,6 +56,64 @@ function link(params) {
         counter--;
         document.onmousewheel=document.onwheel=function(){ 
             return true;
+    };
+}
+
+function next(params) {
+    var speaker_1 = document.getElementById("speaker-1");
+    var speaker_2 = document.getElementById("speaker-2");
+    var speaker_3 = document.getElementById("speaker-3");
+    speaker_1.src = "images/sergey.png";
+    speaker_2.src = "images/maria.png";
+    speaker_3.src = "images/yana.png";
+    speaker_1.onclick = null;
+}
+function prev(params) {
+    var speaker_1 = document.getElementById("speaker-1")
+    var speaker_2 = document.getElementById("speaker-2")
+    var speaker_3 = document.getElementById("speaker-3")
+    speaker_1.src = "images/fillip.png";
+    speaker_2.src = "images/alexandra.png";
+    speaker_3.src = "images/alexey.png";
+    speaker_1.onclick = fillip();
+}
+
+function fillip(params) {
+    var div = document.getElementById("speakers");
+    div.style.transitionProperty = "filter";
+    div.style.transitionDuration = "250ms";
+    div.style.filter = "brightness(40%)";
+    document.onmousewheel=document.onwheel=function(){ 
+        return false;
+    };
+
+    var modal = document.getElementById("my_modal");
+    var btn = document.getElementById("btn_modal_window");
+    var span = document.getElementsByClassName("close_modal_window")[0];
+   
+    btn.onclick = function () {
+       modal.style.display = "block";
+    }
+   
+    span.onclick = function () {
+       modal.style.display = "none";
+    }
+   
+    window.onclick = function (event) {
+       if (event.target == modal) {
+           modal.style.display = "none";
+       }
+   }
+}
+function modal_fillip_close(params) {
+    var div = document.getElementById("speakers");
+    var modal = document.getElementById("modal-fillip")
+    modal.style.display = "none";
+    div.style.transitionProperty = "filter";
+    div.style.transitionDuration = "250ms";
+    div.style.filter = "brightness(100%)";
+    document.onmousewheel=document.onwheel=function(){ 
+        return true;
     };
 }
 
