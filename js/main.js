e6bcc6e70bfdab52b39403fe615b9a5118c5cdcd
Kurpenov Kuat <kurpenov2003@mai.ru>
2020-09-27 00:15:58 +0600

    Fix menu

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ function menu(params) {
         menu.style.zIndex = "999";
         modal.style.zIndex = "1";
         menu.src = "images/close.png";
+        modal.style.filter = "brightness(100%)";
         head.style.transitionProperty = "filter";
         head.style.transitionDuration = "250ms";
         head.style.filter = "brightness(40%)";
@@ -66,7 +67,6 @@ function next(params) {
     speaker_1.src = "images/sergey.png";
     speaker_2.src = "images/maria.png";
     speaker_3.src = "images/yana.png";
-    speaker_1.onclick = null;
 }
 function prev(params) {
     var speaker_1 = document.getElementById("speaker-1")
@@ -75,10 +75,11 @@ function prev(params) {
     speaker_1.src = "images/fillip.png";
     speaker_2.src = "images/alexandra.png";
     speaker_3.src = "images/alexey.png";
-    speaker_1.onclick = fillip();
 }
 
 function fillip(params) {
+    var modal = document.getElementById("modal-fillip");
+    var btn = document.getElementsByClassName("fillip")
     var div = document.getElementById("speakers");
     div.style.transitionProperty = "filter";
     div.style.transitionDuration = "250ms";
@@ -86,24 +87,14 @@ function fillip(params) {
     document.onmousewheel=document.onwheel=function(){ 
         return false;
     };
-
-    var modal = document.getElementById("my_modal");
-    var btn = document.getElementById("btn_modal_window");
-    var span = document.getElementsByClassName("close_modal_window")[0];
-   
     btn.onclick = function () {
        modal.style.display = "block";
     }
-   
-    span.onclick = function () {
-       modal.style.display = "none";
-    }
-   
     window.onclick = function (event) {
        if (event.target == modal) {
            modal.style.display = "none";
        }
-   }
+    }
 }
 function modal_fillip_close(params) {
     var div = document.getElementById("speakers");
